68ab2aa2c706bb49582f7d43d040aff4d02cf54d
Girish Kumar <girish.kumar3517@gmail.com>
2026-07-30 02:21:18 +0530

    Student Score Predction ML Project

--- End to End ML Projects/Student Score Prediction/app.py
+++ End to End ML Projects/Student Score Prediction/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template,url_for
 import numpy as np
 import pandas as pd
+import os
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
@@ -43,4 +44,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
+    app.run(host="0.0.0.0",debug=False)        
